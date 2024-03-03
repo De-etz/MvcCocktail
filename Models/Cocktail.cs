1b444a9ad2dda0108a7cf089baaa2aecea039c9c
Deetz <dialadityasingh@gmail.com>
2024-03-03 11:50:33 -0600

    Made Strength and Rating optional, and name required

--- Models/Cocktail.cs
+++ Models/Cocktail.cs
@@ -5,11 +5,11 @@ namespace MvcCocktail.Models;
 public class Cocktail
 {
     public int Id { get; set; }
-    public string? Name { get; set; }
+    public string Name { get; set; }
     [Range(1, 5)]
-    public int Rating { get; set; }
+    public int? Rating { get; set; }
     [Range(1, 3)]
-    public int Strength { get; set; }
+    public int? Strength { get; set; }
     public bool Shaken { get; set; }
     // public 
     public string? Ingredients { get; set; }
